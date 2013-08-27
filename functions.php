49601c3b4782f7c00bac45cd64071c848b5584d1
Daigo Fujiwara <daigo@daigofujiwara.com>
2013-08-27 08:35:58 -0400

    adding zurb's responsive tables

--- functions.php
+++ functions.php
@@ -62,13 +62,34 @@ function viewport_responsive_meta() {
 }
 add_action('wp_head', 'viewport_responsive_meta');
 
-
+/* media query polyfill for IE8 */
 function respond_scripts() {?>
 <!--[if lt IE 9]>
     <script src="<?php echo get_bloginfo('stylesheet_directory'); ?>/js/respond.min.js" type="text/javascript"></script>
 <![endif]-->
 <?php }
 add_action('wp_head','respond_scripts');
+
+
+
+/* Adding zurb responsive table script, php method taken from here:
+http://codex.wordpress.org/Function_Reference/wp_enqueue_script
+*/
+add_action( 'wp_enqueue_scripts', 'child_add_scripts' );
+
+/**
+ * Register and enqueue a script that does not depend on a JavaScript library.
+ */
+function child_add_scripts() {
+    wp_register_script(
+        'responsive-tables',
+        get_stylesheet_directory_uri() . '/js/responsive-tables.js',
+        false,
+        '1.0',
+        true
+    );
+    wp_enqueue_script( 'responsive-tables' );
+}
 
 
 ?>
